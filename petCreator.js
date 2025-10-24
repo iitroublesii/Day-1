566f51b923d19060814d4f4ee911e4aafe1d17dd
Jo Greenlee <sweettroubles29@gmail.com>
2025-10-24 16:24:53 -0500

    Added to pets.js and petCreator.js

--- petCreator.js
+++ petCreator.js
@@ -1,2 +1,40 @@
 // Proof of Life
 console.log("petCreator.js");
+
+// Factory Function: createPet
+function createPet(name, age, species) {
+    console.log("Create a pet:", name);
+
+    return {
+        name,
+        age,
+        species,
+        energy: 50,
+        happiness: 50,
+
+        // Current status
+        status() {
+            console.log(
+                `${this.name} the ${this.species} is ${this.age} years old. 🐾`
+            );
+            console.log(`Energy: ${this.energy}, Happiness: ${this.happiness}`);
+        },
+
+        // Increases energy
+        feed() {
+            this.energy += 10;
+            console.log(`${this.name} has eaten! Energy is now ${this.energy}.`);
+        },
+
+        // Increases happiness but decreases energy
+        play() {
+            if (this.energy > 0) {
+                this.happiness += 10;
+                this.energy -= 5;
+                console.log(`${this.name} played! Happiness: ${this.happiness}, Energy: ${this.energy}`);
+            } else {
+                console.log(`${this.name} is too sleepy to play.`);
+            }
+        },
+    };
+}
--- pets.js
+++ pets.js
@@ -1,2 +1,32 @@
 // Proof of Life
 console.log("pets.js");
+
+// Global state object
+const state = {
+    pets: [],
+};
+
+testPet.status();
+
+// Button & add event listener
+const addPetButton = document.getElementById("addPet");
+
+addPetButton.addEventListener("click", () => {
+    console.log("Add Pet button clicked!");
+
+    const name = document.getElementById("petName").value.trim();
+    const age = parseInt(document.getElementById("petAge").value.trim());
+    const species = document.getElementById("petSpecies").value.trim();
+
+    console.log("Form Inputs:", name, age, species);
+
+    // Create a new pet
+    const newPet = createPet(name, age, species);
+
+    // Add to global state array
+    state.pets.push(newPet);
+
+    // Proof of life:
+    newPet.status();
+    console.table(state.pets);
+});
